bfdf69c88b62af1056704726535bee91f1d4682f
Mladan Ilic <116818129+djo1975@users.noreply.github.com>
2023-01-04 21:53:48 +0100

    liners fix

--- js/validation.js
+++ js/validation.js
@@ -2,7 +2,7 @@ const formButton = document.querySelector('#form-button');
 function validate() {
   const text = document.getElementById('email').value;
   const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-  document.forms[0].onsubmit = function (e) {
+  document.forms[0].onsubmit = function email(e) {
     if (!(validator.test(text))) {
       document.getElementById('error-message').innerHTML = 'Please use lowercase to enter valid email address!';
       document.getElementById('error-message').style.visibility = 'visible';
@@ -13,6 +13,3 @@ function validate() {
     }
   };
 }
-formButton.addEventListener('click', () => {
-  validate();
-});
