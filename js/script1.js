2d0b90fc6e527240d60e24fd1229286dbe4d9c89
djo1975 <mladanilic1975@gmail.com>
2023-07-15 16:54:07 +0200

    Done

--- js/script1.js
+++ js/script1.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const languagesMenu = document.querySelector('.languagesxx');
   const arrowLanguages = document.querySelector('.others-arrow');
-  const frameworksMenu = document.querySelector('.framework');
+  const frameworksMenu = document.querySelector('.frameworksxx');
   const arrowFrameworks = document.querySelector('.others-arrow-1');
-  const frameworksList = document.querySelector('.frameworksxx');
-  const skillsMenu = document.querySelector('.skills');
+  const skillsMenu = document.querySelector('.skillsxx');
   const arrowSkills = document.querySelector('.others-arrow-2');
-  const skillsList = document.querySelector('.skillsxx');
 
   arrowLanguages.addEventListener('click', () => {
     languagesMenu.classList.toggle('open');
@@ -16,19 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
   arrowFrameworks.addEventListener('click', () => {
     frameworksMenu.classList.toggle('open');
     arrowFrameworks.classList.toggle('open');
-    frameworksList.classList.toggle('open');
   });
 
   arrowSkills.addEventListener('click', () => {
     skillsMenu.classList.toggle('open');
     arrowSkills.classList.toggle('open');
-    skillsList.classList.toggle('open');
   });
 });
-document.addEventListener('DOMContentLoaded', () => {
+
   const resumeButton = document.querySelector('.resume');
 
   resumeButton.addEventListener('click', () => {
     window.open('https://docs.google.com/document/d/14NXJBul3J7lE1JGAivwDjxdtZ9ThdYaYddvdL_k_C9M/edit?usp=sharing', '_blank');
   });
-});
